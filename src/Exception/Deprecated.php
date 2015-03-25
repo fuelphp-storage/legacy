1258e9d0018be1ec64ee5bcf45065b8f658922c1
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-03-25 10:57:27 +0100

    Adds fieldset proxy and deprecated exception

---
+++ src/Exception/Deprecated.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    Fuel\Legacy
+ * @version    2.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2015 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Legacy\Exception;
+
+/**
+ * Indicates a feature has been deprecated when using the v2 -> v1 interface
+ */
+class Deprecated extends \Exception
+{
+
+}
